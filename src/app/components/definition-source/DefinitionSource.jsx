22832801c4998962b7726c0a27d01c214011d83d
ishu <ishukhatri@users.noreply.github.com>
2023-06-08 10:35:05 +0530

    fixed desktop view

--- src/app/components/definition-source/DefinitionSource.jsx
+++ src/app/components/definition-source/DefinitionSource.jsx
@@ -3,17 +3,22 @@ import Image from "next/image";
 
 const DefinitionSource = ({ link }) => {
   return (
-    <div className="text-sm font-normal leading-4 underline">
-      <p className="text-grey-dark leading-[17px] border-grey-dark dark:border-grey-dark">
-        Source
-      </p>
-      <div className="mt-2 flex items-center text-black-dark">
-        <p className="dark:text-white text-black-dark">{link}</p>
+    <div className="flex flex-col space-y-2 md:flex-row md:space-x-6 md:space-y-0 text-sm font-normal leading-4">
+      <p className="text-grey-dark  dark:border-grey-dark">Source</p>
+      <div className="flex items-center text-black-dark">
+        <a
+          href={link}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="dark:text-white text-black-dark underline"
+        >
+          {link}
+        </a>
         <Image
           src="/assets/new-window.svg"
           width={12}
           height={12}
-          className="w-3 h-3 ml-[9px]"
+          className="w-3 h-3 ml-2"
         />
       </div>
     </div>
