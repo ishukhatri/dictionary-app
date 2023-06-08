e274b4a26b6ee91a00a34351d9cdf70f72ac5ec8
ishu <ishukhatri@users.noreply.github.com>
2023-06-08 08:27:01 +0530

    fixed viewport per story basis

--- src/app/components/definition-source/DefinitionSource.stories.jsx
+++ src/app/components/definition-source/DefinitionSource.stories.jsx
@@ -5,6 +5,28 @@ export default {
   component: DefinitionSource,
 };
 
-export const Default = () => (
+export const mobile = () => (
   <DefinitionSource link="https://en.wiktionary.org/wiki/keyboard" />
 );
+mobile.parameters = {
+  viewport: {
+    defaultViewport: "mobile",
+  },
+};
+export const tablet = () => (
+  <DefinitionSource link="https://en.wiktionary.org/wiki/keyboard" />
+);
+tablet.parameters = {
+  viewport: {
+    defaultViewport: "tablet",
+  },
+};
+
+export const desktop = () => (
+  <DefinitionSource link="https://en.wiktionary.org/wiki/keyboard" />
+);
+desktop.parameters = {
+  viewport: {
+    defaultViewport: "desktop",
+  },
+};
