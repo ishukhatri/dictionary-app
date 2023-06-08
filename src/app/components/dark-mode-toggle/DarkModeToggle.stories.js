7b1e5f04b83f1cffdbcd1a6355a4c07d9ac73340
ishu <ishukhatri@users.noreply.github.com>
2023-06-08 11:35:30 +0530

    renamed toggle

---
+++ src/app/components/dark-mode-toggle/DarkModeToggle.stories.js
@@ -0,0 +1,8 @@
+import Toggle from "./DarkModeToggle"; // Import your Toggle component
+
+export default {
+  title: "Components/Toggle",
+  component: Toggle,
+};
+
+export const Default = () => <Toggle />;
