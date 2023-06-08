7e3d5af092792adaa2b754609707d977820eda3d
ishu <ishukhatri@users.noreply.github.com>
2023-06-08 11:15:03 +0530

    synonyms fix

--- src/app/components/synonym/Synonym.jsx
+++ src/app/components/synonym/Synonym.jsx
@@ -2,13 +2,11 @@ import React from "react";
 
 const Synonym = ({ synonyms }) => {
   return (
-    <div className="flex flex-wrap">
-      <p className="text-grey-dark font-normal text-base leading-[19px] mr-6">
-        Synonyms
-      </p>
+    <div className="flex flex-wrap text-base md:text-xl">
+      <p className="text-grey-dark font-normal leading-5 mr-6">Synonyms</p>
       {synonyms.map((synonym, index) => (
         <p
-          className="text-purple-bright font-bold text-base leading-[19px] whitespace-nowrap leading-5"
+          className="text-purple-bright font-bold whitespace-nowrap leading-5"
           key={index}
         >
           {synonym}
--- src/app/components/synonym/Synonym.stories.jsx
+++ src/app/components/synonym/Synonym.stories.jsx
@@ -8,23 +8,16 @@ export default {
 
 const synonyms = ["Word1", "Word2", "Word3"];
 
-export const Default = () => <Synonym synonyms={synonyms} />;
+export const mobile = () => <Synonym synonyms={synonyms} />;
+mobile.parameters = {
+  viewport: {
+    defaultViewport: "mobile",
+  },
+};
 
-export const EmptySynonyms = () => <Synonym synonyms={[]} />;
-
-export const LongList = () => {
-  const longList = [
-    "Word1",
-    "Word2",
-    "Word3",
-    "Word4",
-    "Word5",
-    "Word6",
-    "Word7",
-    "Word8",
-    "Word9",
-    "Word10",
-  ];
-
-  return <Synonym synonyms={longList} />;
+export const nonMobile = () => <Synonym synonyms={synonyms} />;
+nonMobile.parameters = {
+  viewport: {
+    defaultViewport: "desktop",
+  },
 };
