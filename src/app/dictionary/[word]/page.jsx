c90267f898dd3d7042c8791a616e2b532485674d
ishu <ishukhatri@users.noreply.github.com>
2023-06-25 17:58:01 +0530

    fixed metadata

--- src/app/dictionary/[word]/page.jsx
+++ src/app/dictionary/[word]/page.jsx
@@ -1,9 +1,7 @@
 import Result from "@components/main/result/Result";
 
 const WordPage = async ({ params }) => {
-  console.log("params", params);
   const word = params.word;
-  console.log("word in effect is :", word);
 
   return <Result word={word} />;
 };
