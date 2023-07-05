593d18d6f23ee1bfa1cbce2084640a17b9949399
ishu <ishukhatri@users.noreply.github.com>
2023-07-05 12:53:02 +0530

    fixed synonyms ui

--- src/app/components/main/result/meanings/meaningItem/synonyms/Synonyms.jsx
+++ src/app/components/main/result/meanings/meaningItem/synonyms/Synonyms.jsx
@@ -2,13 +2,10 @@ import React from "react";
 
 const Synonyms = ({ synonyms }) => {
   return (
-    <div className="flex flex-wrap text-base md:text-xl">
-      <p className="text-grey-dark font-normal leading-5 mr-6">Synonyms</p>
+    <div className="flex flex-wrap text-base md:text-xl leading-normal">
+      <p className="text-grey-dark font-normal mr-6">Synonyms</p>
       {synonyms.map((synonym, index) => (
-        <p
-          className="text-purple-bright font-bold whitespace-nowrap leading-5"
-          key={index}
-        >
+        <p className="text-purple-bright font-bold mr-2" key={index}>
           {synonym}
           {index !== synonyms.length - 1 && ", "}{" "}
           {/* Add comma if not the last item */}
